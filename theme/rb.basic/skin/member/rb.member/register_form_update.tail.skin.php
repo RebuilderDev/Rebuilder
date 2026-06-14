d2966cf624d1ef5efc6616f3bde3331e7fa71d34
RebuilderDev <cs@rebuilder.co.kr>
2026-06-15 08:44:00 +0900

    PHP8.X 에러구문 대응 (2.2.6.2)

--- theme/rb.basic/skin/member/rb.member/register_form_update.tail.skin.php
+++ theme/rb.basic/skin/member/rb.member/register_form_update.tail.skin.php
@@ -2,10 +2,14 @@
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
 //추가정보 저장
+$rb_post_mb_partner = isset($_POST['mb_partner']) ? (int)$_POST['mb_partner'] : 0;
+$rb_post_mb_bank = isset($_POST['mb_bank']) ? sql_escape_string($_POST['mb_bank']) : '';
+$rb_post_re = isset($_POST['re']) ? $_POST['re'] : '';
+
 if($w == "") {
     if(isset($pa['pa_is']) && $pa['pa_is'] == 1 && isset($pa['pa_use']) && $pa['pa_use'] == 1) {
 
-        if($_POST['mb_partner'] == 2 || $_POST['mb_partner'] == 1) {
+        if($rb_post_mb_partner == 2 || $rb_post_mb_partner == 1) {
 
             if(isset($pa['pa_add_use']) && $pa['pa_add_use'] == 1) {
 
@@ -26,9 +30,9 @@ if($w == "") {
 
 
             $sqls = "UPDATE {$g5['member_table']}
-                        SET mb_partner = '{$_POST['mb_partner']}',
+                        SET mb_partner = '{$rb_post_mb_partner}',
                             mb_partner_add_time = '" . G5_TIME_YMDHIS . "',
-                            mb_bank = '{$_POST['mb_bank']}',
+                            mb_bank = '{$rb_post_mb_bank}',
                             mb_level = '{$re_level}'
                             where mb_id = '{$mb_id}' ";
             sql_query($sqls);
@@ -38,14 +42,14 @@ if($w == "") {
 
     if(isset($pa['pa_is']) && $pa['pa_is'] == 1 && isset($pa['pa_use']) && $pa['pa_use'] == 1) {
 
-        if($_POST['mb_partner'] == 2 || $_POST['mb_partner'] == 1) {
+        if($rb_post_mb_partner == 2 || $rb_post_mb_partner == 1) {
 
             $sqls = "UPDATE {$g5['member_table']}
-                        SET mb_bank = '{$_POST['mb_bank']}'
+                        SET mb_bank = '{$rb_post_mb_bank}'
                             where mb_id = '{$mb_id}' ";
             sql_query($sqls);
 
-            if(isset($_POST['re']) && $_POST['re'] == "re") {
+            if($rb_post_re == "re") {
 
                 if(isset($pa['pa_add_use']) && $pa['pa_add_use'] == 1) {
 
@@ -68,14 +72,14 @@ if($w == "") {
                 }
 
                 $sqls = "UPDATE {$g5['member_table']}
-                        SET mb_partner = '{$_POST['mb_partner']}',
+                        SET mb_partner = '{$rb_post_mb_partner}',
                             mb_partner_add_time = '" . G5_TIME_YMDHIS . "',
-                            mb_bank = '{$_POST['mb_bank']}',
+                            mb_bank = '{$rb_post_mb_bank}',
                             mb_level = '{$re_level}'
                             where mb_id = '{$mb_id}' ";
                 sql_query($sqls);
 
-                goto_url(G5_HTTP_BBS_URL.'/register_result.php?partner='.$_POST['mb_partner']);
+                goto_url(G5_HTTP_BBS_URL.'/register_result.php?partner='.$rb_post_mb_partner);
             }
 
         }
@@ -84,21 +88,34 @@ if($w == "") {
 
 }
 
+if ($w == "" && function_exists('rb_sms_cert_is_enabled') && rb_sms_cert_is_enabled() && get_session('ss_rb_sms_cert_verified') === '1') {
+    $rb_sms_cert_hp = function_exists('rb_sms_cert_normalize_hp') ? rb_sms_cert_normalize_hp(get_session('ss_rb_sms_cert_hp')) : preg_replace('/[^0-9]/', '', (string)get_session('ss_rb_sms_cert_hp'));
+    if ($rb_sms_cert_hp !== '') {
+        sql_query("UPDATE {$g5['member_table']}
+            SET mb_hp = '" . sql_escape_string(hyphen_hp_number($rb_sms_cert_hp)) . "',
+                mb_certify = 'hp'
+            WHERE mb_id = '" . sql_escape_string($mb_id) . "'");
+    }
+}
+
 //----------------------------------------------------------
 // SMS 문자전송 시작
 //----------------------------------------------------------
 
 
-$sms_contents = $default['de_sms_cont1'];
+$sms_contents = isset($default['de_sms_cont1']) ? $default['de_sms_cont1'] : '';
 $sms_contents = str_replace("{이름}", $mb_name, $sms_contents);
 $sms_contents = str_replace("{회원아이디}", $mb_id, $sms_contents);
-$sms_contents = str_replace("{회사명}", $default['de_admin_company_name'], $sms_contents);
+$sms_contents = str_replace("{회사명}", isset($default['de_admin_company_name']) ? $default['de_admin_company_name'] : '', $sms_contents);
 
 // 핸드폰번호에서 숫자만 취한다
 $receive_number = preg_replace("/[^0-9]/", "", $mb_hp);  // 수신자번호 (회원님의 핸드폰번호)
-$send_number = preg_replace("/[^0-9]/", "", $default['de_admin_company_tel']); // 발신자번호
+$send_number = preg_replace("/[^0-9]/", "", isset($default['de_admin_company_tel']) ? $default['de_admin_company_tel'] : ''); // 발신자번호
 
-if ($w == "" && $default['de_sms_use1'] && $receive_number)
+$rb_de_sms_use1 = !empty($default['de_sms_use1']);
+$rb_de_admin_company_name = isset($default['de_admin_company_name']) ? $default['de_admin_company_name'] : '';
+
+if ($w == "" && $rb_de_sms_use1 && $receive_number)
 {
 	if ($config['cf_sms_use'] == 'icode')
 	{
@@ -115,7 +132,7 @@ if ($w == "" && $default['de_sms_use1'] && $receive_number)
                 $strDest     = array();
                 $strDest[]   = $receive_number;
                 $strCallBack = $send_number;
-                $strCaller   = iconv_euckr(trim($default['de_admin_company_name']));
+                $strCaller   = iconv_euckr(trim($rb_de_admin_company_name));
                 $strSubject  = '';
                 $strURL      = '';
                 $strData     = iconv_euckr($sms_contents);
