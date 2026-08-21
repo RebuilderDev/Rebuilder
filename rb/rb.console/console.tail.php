15df06a925f3cfda3b480caf17c326306deae971
RebuilderDev <cs@rebuilder.co.kr>
2026-08-22 05:05:18 +0900

    캘린더 표기 동기화

--- rb/rb.console/console.tail.php
+++ rb/rb.console/console.tail.php
@@ -5,27 +5,33 @@
 (function($){
     if (!$ || !$.datepicker) return;
 
-    $.datepicker.setDefaults({
+    var rbConsoleDatepickerDefaults = {
         closeText:'닫기',prevText:'이전달',nextText:'다음달',currentText:'오늘',
         monthNames:['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
         monthNamesShort:['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
         dayNames:['일요일','월요일','화요일','수요일','목요일','금요일','토요일'],
         dayNamesShort:['일','월','화','수','목','금','토'],dayNamesMin:['일','월','화','수','목','금','토'],
-        weekHeader:'주',dateFormat:'yy-mm-dd',firstDay:0,isRTL:false,showMonthAfterYear:true,yearSuffix:'년'
-    });
+        weekHeader:'주',dateFormat:'yy-mm-dd',firstDay:0,isRTL:false,showMonthAfterYear:true,yearSuffix:'년',
+        changeMonth:false,changeYear:false,showButtonPanel:false
+    };
+    $.datepicker.setDefaults(rbConsoleDatepickerDefaults);
 
     function dateLimit($input, name) {
         var value = $.trim(String($input.attr(name) || ''));
         return /^\d{4}-\d{2}-\d{2}$/.test(value) ? value : null;
     }
 
-    window.rbConsoleDatepickerRefresh = function(input) {
-        var $input = $(input);
-        if (!$input.length || !$input.hasClass('hasDatepicker')) return;
-        $input.datepicker('option', {
+    function datepickerOptions($input) {
+        return $.extend({}, rbConsoleDatepickerDefaults, {
             minDate:dateLimit($input, 'min'),
             maxDate:dateLimit($input, 'max')
         });
+    }
+
+    window.rbConsoleDatepickerRefresh = function(input) {
+        var $input = $(input);
+        if (!$input.length || !$input.hasClass('hasDatepicker')) return;
+        $input.datepicker('option', datepickerOptions($input));
     };
 
     function initDatepickers(root) {
@@ -33,18 +39,18 @@
         $root.find('input[type="date"]:not([data-rb-datepicker="off"]), input.datepicker_inp:not([data-rb-datepicker="off"])').addBack('input[type="date"]:not([data-rb-datepicker="off"]), input.datepicker_inp:not([data-rb-datepicker="off"])').each(function(){
             var $input = $(this);
             $input.attr('autocomplete','off');
-            if ($input.hasClass('hasDatepicker')) return;
+            if ($input.hasClass('hasDatepicker')) {
+                $input.datepicker('option', datepickerOptions($input));
+                return;
+            }
             if (String($input.attr('type')).toLowerCase() === 'date') $input.attr('type','text');
-            $input.addClass('datepicker_inp').datepicker({
-                dateFormat:'yy-mm-dd',
-                minDate:dateLimit($input, 'min'),
-                maxDate:dateLimit($input, 'max'),
+            $input.addClass('datepicker_inp').datepicker($.extend({}, datepickerOptions($input), {
                 beforeShow:function(input){
                     var $current = $(input);
                     return {minDate:dateLimit($current, 'min'),maxDate:dateLimit($current, 'max')};
                 },
                 onSelect:function(){ $(this).trigger('input').trigger('change'); }
-            });
+            }));
         });
     }
 
